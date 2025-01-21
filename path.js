eae4f6ac4cd73f3600873d10f24aeece26f76d10
joel12sa <sasigjo3190@gmail.com>
2025-01-20 22:56:04 -0500

    new

--- path.js
+++ path.js
@@ -37,7 +37,7 @@ async function listar(){
         const fileType = isDirectory ? 'd' : 'f'
         const fileSize = filestat.size
         const fileModify= filestat.mtime.toLocaleDateString()
-        return `${fileType} ${file} ${fileSize} bytes ${fileModify}`;
+        return `${pc.bgMagenta(fileType)} ${file} ${fileSize} bytes ${fileModify}`;
     })
 
     const filesInfo=await Promise.all(filesPromises)
